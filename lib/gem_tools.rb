06c4b77cbed49652a63a31d763dacf34381ddb5b
Michael Moen <m.moen@tukaiz.com>
2010-02-11 15:13:11 -0600

    rough cut of windows support

--- lib/gem_tools.rb
+++ lib/gem_tools.rb
@@ -11,6 +11,10 @@ module GemTools
     send(cmd.to_sym) rescue help
   end
 
+  def help
+    puts OPTS
+  end
+
   def setup
     require 'fileutils'
     dest_file = File.join(ARGV[1], '/config/gems.yml')
@@ -23,28 +27,21 @@ module GemTools
     end
   end
 
-  def load_gems
-    config = load_config
-    unless config['gems'].nil?
-      gems = config['gems'].reject {|gem_info| ! gem_info['load'] }
-      gems.each do |gem_info|
-        if defined?(gem)
-          gem gem_info['name'], gem_info['version']
-        else
-          require_gem gem_info['name'], gem_info['version']
-        end
-        require gem_info['require_name'] || gem_info['name']
-      end
+  def install
+    commands.each do |command|
+      ret = system command
+      # something bad happened, pass on the message
+      p $? unless ret
     end
   end
 
-  def help
-    puts OPTS
+  def dryrun
+    puts "\n#{commands.join("\n")}\n\n"
   end
 
   def to_gemfile
     config = load_config
-    
+
     if File.exist?('Gemfile')
       puts "GemFile already exists, not overwriting."
       exit 1
@@ -77,114 +74,95 @@ module GemTools
     end
   end
 
-  def load_config
-    require 'yaml'
-    require 'erb'
-    config_file = find_config
-    unless config_file
-      puts 'Could not find a gems.yml, checked . and ./config'
-      exit 1
-    end
-    YAML.load(ERB.new(File.open(config_file).read).result)
-  end
+  private
 
-  def install
-    require 'rubygems'
-    if RUBY_PLATFORM =~ /MSWIN/
-      puts "gemtools install doesn't currently work in windows. The commands you need to install the gems will be printed out for you.\n"
-      dryrun
-      return
-    else
-      commands.each do |command|
-        ret = system command
-        # something bad happened, pass on the message
-        p $? unless ret
-      end
-    end
-  end
-
-  def dryrun
-    puts "\n#{commands.join("\n")}\n\n"
-  end
-
-  def commands
-    config = load_config
-    gems = config['gems']
-    commands = []
-
-    unless gems.nil?
-      docs = ''
-      unless OPTIONS.has_key?(:docs)
-        docs << '--no-rdoc ' unless (`rdoc --version 2> /dev/null`).nil?
-        docs << '--no-ri ' unless (`ri --version 2> /dev/null`).nil?
-      end
-
-      gem_command = config['gem_command'] || 'gem'
-      gem_dash_y = "1.0" > Gem::RubyGemsVersion ? '-y' : ''
-
-      gems.each do |gem|
-        spec, loaded, version = check_gem(gem_command, gem['name'], gem['version'])
-        # if forced
-        # or the spec version doesn't match the required version
-        # or require_gem returns false
-        #    (return false also happens if the gem has already been loaded)
-        if OPTIONS.has_key?(:force) || !spec || (! loaded || version != gem['version'])
-          gem_config = gem['config'] ? " -- #{gem['config']}" : ''
-          source = gem['source'] || config['source'] || nil
-          source = "--source #{source}" if source
-          cmd = "#{gem_command} install #{gem['path'] || gem['name']} -v #{gem['version']} #{gem_dash_y} #{source} #{docs} #{gem_config}"
-          commands << cmd
-        else
-          puts "#{gem['name']} #{gem['version']} already installed"
+    def load_gems
+      config = load_config
+      unless config['gems'].nil?
+        gems = config['gems'].reject {|gem_info| ! gem_info['load'] }
+        gems.each do |gem_info|
+          if defined?(gem)
+            gem gem_info['name'], gem_info['version']
+          else
+            require_gem gem_info['name'], gem_info['version']
+          end
+          require gem_info['require_name'] || gem_info['name']
         end
       end
     end
-    commands
-  end
 
-  def check_gem(command, name, version='')
-    ENV['PATH'] ||= '/usr/bin'
-    ENV['PATH'] += ':/usr/local/bin:/usr/bin'
-    spec = YAML.load(`PATH=#{ENV['PATH']} #{command} spec #{name} 2> /dev/null`)
-    loaded = false
-    begin
-      loaded = defined?(gem) ? gem(name, version) : require_gem(name, version)
-      version = spec.version.version
-    rescue Exception => e
-      # puts e
+    def load_config
+      require 'yaml'
+      require 'erb'
+      config_file = find_config
+      unless config_file
+        puts 'Could not find a gems.yml, checked . and ./config'
+        exit 1
+      end
+      YAML.load(ERB.new(File.open(config_file).read).result)
     end
-    [spec, loaded, version]
-  end
 
-  def find_config
-    %w( . config ).each do |dir|
-      config_file = File.join(dir, 'gems.yml')
-      return config_file if File.exist?(config_file)
+    def commands
+      config = load_config
+      gems = config['gems']
+      commands = []
+
+      unless gems.nil?
+        docs = ''
+        unless OPTIONS.has_key?(:docs)
+          docs << '--no-rdoc ' unless (`rdoc --version 2> /dev/null`).nil?
+          docs << '--no-ri ' unless (`ri --version 2> /dev/null`).nil?
+        end
+
+        gem_command = config['gem_command'] || 'gem'
+        gem_dash_y = "1.0" > Gem::RubyGemsVersion ? '-y' : ''
+
+        gems.each do |gem|
+          meets_reqs =
+
+          # if forced
+          # or the installed gems don't meet requirements
+          if OPTIONS.has_key?(:force) || meets_requirements?(gem['name'], gem['version'])
+            gem_config = gem['config'] ? " -- #{gem['config']}" : ''
+            source = gem['source'] || config['source'] || nil
+            source = "--source #{source}" if source
+            cmd = "#{gem_command} install #{gem['path'] || gem['name']} -v #{gem['version']} #{gem_dash_y} #{source} #{docs} #{gem_config}"
+            commands << cmd
+          else
+            puts "#{gem['name']} #{gem['version']} already installed"
+          end
+        end
+      end
+      commands
     end
-    nil
-  end
 
-  def has_gem?(gem)
-    @gems ||= Gem::SourceIndex.from_installed_gems
-    @gems.search(gem['name'], gem['version']).empty?
-  end
-
-  def check_updates
-    config = load_config
-    gems = config['gems']
-
-    gems.each do |gem|
-      source = gem['source'] || config['source'] || nil
-      source = "--source #{source}" if source
-      # puts "Checking #{gem['name']}"
-      out = `gem list -r #{gem['name']} #{source}`
-      ver = out.match(/\(([0-9\.]+)/)[1] #rescue 0
-      puts "#{gem['name']} #{ver} is available, #{gem['version']} installed" if ver > gem['version']
+    def find_config
+      %w( . config ).each do |dir|
+        config_file = File.join(dir, 'gems.yml')
+        return config_file if File.exist?(config_file)
+      end
+      nil
     end
-  end
-  
-  private
-    
+
+    def meets_requirements?(name, version)
+      @specs ||= Gem::SourceIndex.from_installed_gems
+      @specs.find_name(name, Gem::Requirement.new(version)).empty?
+    end
+
+    def check_updates
+      config = load_config
+      gems = config['gems']
+
+      gems.each do |gem|
+        source = gem['source'] || config['source'] || nil
+        source = "--source #{source}" if source
+        # puts "Checking #{gem['name']}"
+        out = `gem list -r #{gem['name']} #{source}`
+        ver = out.match(/\(([0-9\.]+)/)[1] #rescue 0
+        puts "#{gem['name']} #{ver} is available, #{gem['version']} installed" if ver > gem['version']
+      end
+    end
+
     def bundler_gem_line(gem)
       line = "gem '#{gem['name']}', '#{gem['version']}'"
       line += ", :require => '#{gem['require_name']}'" if gem['require_name']
